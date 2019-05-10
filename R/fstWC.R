f97f23c84e0655cce4bb16811c7044f66a657989
j-a-thia <joshua.a.thia@gmail.com>
2019-05-10 23:56:05 +1000

    fstWC & plot_missingBYsamples
    fstWC -- added a dist matrix argument
    
    plot_missingBYsamples -- started code for function to plot missing data stats for samples

--- R/fstWC.R
+++ R/fstWC.R
@@ -9,17 +9,23 @@
 #' @param pairs Logical: Should pairwise FSTwc be calculated (TRUE) or the
 #' among population FSTwc (FALSE)?
 #'
+#' @param dist Logical: Should a a distance matrix of FST be returned as well?
+#' Will only run if \code{pairs==TRUE}.
+#'
 #' @return A list with two indices. \code{$fst.mean} contains the mean FST. In pairwise analyses,
 #' this is a data table with population pairs. \code{$fst.locus} contains the locus-specific
 #' FST values. If analysis was not pairwise, this is a vector, but for pairwise analyses,
-#' this is a data table with population pairs.
+#' this is a data table with population pairs. Additionally, if a pairwise analysis
+#' was run and a distance matrix requested (\code{pairs==TRUE} and \code{dist==TRUE}),
+#' the output can be found in \code{$fst.dist}.
 #'
 #' @references
 #' Weir, Cockerham (1984) Estimating F-statistics for the analysis of population structure. Evol. \cr
 #' Weir, Hill (2002) Estimating F-statistics. Annu. Rev. Genet
 #'
 #' @examples
-#' freqMat <- genomaliciousFreqs
+#' data(genomalicious_Freqs)
+#' freqMat <- genomalicious_Freqs
 #' sampMat <- matrix(rep(30, 32), nrow=4, ncol=8)
 #' rownames(sampMat) <- paste0('Pop', 1:4)
 #' colnames(sampMat) <- colnames(freqMat); rownames(sampMat) <- rownames(freqMat)
@@ -28,7 +34,7 @@
 #' fstWC(freqMat, sampMat, pairs=TRUE)
 #'
 #' @export
-fstWC <- function(freqMat, sampMat, pairs=FALSE){
+fstWC <- function(freqMat, sampMat, pairs=FALSE, dist=FALSE){
   # --------------------------------------------+
   # Libraries and assertions
   # --------------------------------------------+
@@ -52,6 +58,11 @@ fstWC <- function(freqMat, sampMat, pairs=FALSE){
     stop('Make sure all column names in freqMat are also in sampMat.')
   }
 
+  if(dist==TRUE & pairs==FALSE){
+    warning('Argument dist set to TRUE, but pairs set to FALSE: Will
+    only calcualte a distance matrix for pairwise analyses.')
+  }
+
   # Make sure sampMat and freqMat are in the same order row-wise
   sampMat <- sampMat[rownames(freqMat), ]
 
@@ -62,13 +73,13 @@ fstWC <- function(freqMat, sampMat, pairs=FALSE){
     # Calculate variance per locus
     lociVar <- fstWC_varcomps(freqMat, sampleMat)
     # Theta across loci
-    thetaMean <- sum(lociVar$NUMER) / sum(lociVar$DENOM)
+    fst.mean <- sum(lociVar$NUMER) / sum(lociVar$DENOM)
     # Theta per locus
-    thetaLocus <- lociVar$NUMER / lociVar$DENOM
-    names(thetaLocus) <- lociVar$LOCUS
+    fst.locus <- lociVar$NUMER / lociVar$DENOM
+    names(fst.locus) <- lociVar$LOCUS
 
-    # Return
-    return(list(fst.mean=thetaMean, fst.locus=thetaLocus))
+    # Return among population analyses
+    return(list(fst.mean=fst.mean, fst.locus=fst.locus))
 
   } else if(pairs==TRUE){
     pairCombos <- combn(x=rownames(freqMat), m=2)
@@ -89,10 +100,28 @@ fstWC <- function(freqMat, sampMat, pairs=FALSE){
       return(list(mean=thetaMean, bylocus=thetaLocus))
     })
 
-    # Return
-    return(list(fst.mean=do.call('rbind', lapply(pairLs, function(X){ X$mean}))
-                , fst.locus=do.call('rbind', lapply(pairLs, function(X){ X$bylocus}))
-                ))
+    # Create a list of objects to return for pairwise analyses
+    outLs <- list(fst.mean=do.call('rbind', lapply(pairLs, function(X){ X$mean}))
+                    , fst.locus=do.call('rbind', lapply(pairLs, function(X){ X$bylocus}))
+    )
+
+    # If an FST distance matrix was requested:
+    if(dist==TRUE){
+      # Empty matrix
+      fst.dist <- matrix(0, nrow=nrow(freqMat), ncol=nrow(freqMat)
+                         , dimnames=list(rownames(freqMat), rownames(freqMat)))
+      # Get the pairwise info and fill the distance matrix
+      for(j in 1:ncol(pairCombos)){
+        pop1 <- pairCombos[1,j]
+        pop2 <- pairCombos[2,j]
+        fst.dist[pop2, pop1] <- outLs <- fst.mean[POP1==pop1 & POP2==pop2]$FST
+      }
+      # Add to the output list
+      outLs$fst.dist <- as.dist(fst.dist, diag=TRUE)
+    }
+
+    # Return pairwise analyses
+    return(outLs)
 
   }
 }
