0b73ccfd5657dc51df2754127b78c72df4c73b3c
j-a-thia <12246975+j-a-thia@users.noreply.github.com>
2020-01-12 11:02:52 +1100

    new function: miss_sim_structure
    Simulate a missing datastructure in clean observed data

---
+++ R/genomalicious_PatchyGTs.R
@@ -0,0 +1,16 @@
+#' An example of a patchy genotype matrix
+#'
+#' A matrix matrix of genotypes with missing data.
+#'
+#' @usage data(genomalicious_PatchyMat)
+#'
+#' @format A matrix with 100 rows and 100 columns.
+#'
+#' @details Samples IDs are in \code{rownames(genomalicious_PatchyGTs)}, and locus
+#' IDs are in \code{colnames(genomalicious_PatchyGTs)}. Missing data are
+#' coded by \code{NA.}
+#'
+#' @docType data
+#' @keywords datasets
+#' @name genomalicious_PatchyGTs
+NULL
