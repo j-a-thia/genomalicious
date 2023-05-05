54c3f24d4275c6971b9b89b30051aa4794fe5c66
Preston J McDonald <43688190+PrestonMcDonald@users.noreply.github.com>
2023-05-05 12:50:37 -0400

    Update vcf2DT.R

--- R/vcf2DT.R
+++ R/vcf2DT.R
@@ -131,15 +131,15 @@ vcf2DT <- function(vcfFile, dropCols=NULL, keepComments=FALSE, keepInfo=FALSE){
 
   # ... Separate by $FORMAT names
   vcfDT <- vcfDT[, !'FORMAT'] %>%
-    as.data.table(
       separate(col='DATA', into=formatNames, sep=':')
-    )
 
   # .... Replace '.' values in the data columns with NA
   for(f in formatNames){
     vcfDT[[f]][vcfDT[[f]]=='.'] <- NA
   }
 
+  vcfDT <- as.data.table(vcfDT)
+  
   # Make sure DP and RO integers
   if('DP' %in% colnames(vcfDT)){ vcfDT[, DP:=as.integer(DP)] }
   if('RO' %in% colnames(vcfDT)){ vcfDT[, RO:=as.integer(RO)] }
